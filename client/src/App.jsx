8b393444640cd4cf6c74a5176d10d6729e577083
priyabrata1122 <priyabratass64@gmail.com>
2025-07-21 20:19:58 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from './components/ui/button'
+
+function App() {
+  
+  return (
+    <>
+      <Button variant="secondary">Click me</Button>
+    </>
+  )
+}
+
+export default App
