f4e82213dcccbc2d0c0f24e9b3c6b718494eab20
Srikanta Barai <sbarai3210@gmail.com>
2025-08-14 02:16:30 +0530

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+  base: "/",
+  build: { outDir: "dist" }
+});
