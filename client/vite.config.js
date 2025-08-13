fd56996d9a2efbdfb6d9482c646d6f1befd137f8
Srikanta Barai <sbarai3210@gmail.com>
2025-08-14 00:34:46 +0530

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://campusstay-3v5w.onrender.com',
         changeOrigin: true,
         secure: false,
       }
