81e18a329a05c2d397ae931a6ed29655ef56c834
Srikanta Barai <sbarai3210@gmail.com>
2025-08-14 01:13:01 +0530

    final

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BACKEND_URL,
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       }
